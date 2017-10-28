cdda5e28627edd7c8ca90cc3b0689560ab4d6585
hanzhihong9 <hanzhihong9@gmail.com>
2017-10-28 19:07:41 -0400

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,40 @@
+'''
+>>> it= (i for i in range(1,3))
+>>> it
+<generator object <genexpr> at 0x0000000004D36438>
+>>> it.next()
+1
+>>> it.next()
+2
+>>> it.next()
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+StopIteration
+>>> it= [i for i in range(1,4)]
+>>> it
+[1, 2, 3]
+
+
+>>> it= [i for i in range(1,4)]
+>>> m = [[x*y for y in it] for x in it ]
+>>> m
+[[1, 2, 3], [2, 4, 6], [3, 6, 9]]
+
+
+
+>>> it= (i for i in range(1,4))
+>>> m = [[x*y for y in it] for x in it ]
+>>> m
+[[2, 3]]
+
+'''
+
+
+'''
+https://blog.sideci.com/about-style-guide-of-python-and-linter-tool-pep8-pyflakes-flake8-haking-pyling-7fdbe163079d
+5 different tools which are
+pep8, pyflakes, flake8, haking, Pylint.
+
+it is better to user Pycharm , Elipse , VS code, so you do no need to care about the tools.
+they should already includ these tools
+'''
