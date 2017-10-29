9a01f5107d151d22288b74377200e3a632f6f94b
hanzhihong9 <hanzhihong9@gmail.com>
2017-10-29 09:39:31 -0400

    Update some-snepit.py

--- some-snepit.py
+++ some-snepit.py
@@ -193,3 +193,20 @@ It is important to note that you are not limited to functions as decorators. A d
 
 You may also want to take a closer look at Python’s functools module. There is much to discover there!
 '''
+
+'''
+content management
+'''
+
+class CustomOpen(object):
+    def __init__(self, filename):
+        self.file = open(filename)
+
+    def __enter__(self):
+        return self.file
+
+    def __exit__(self, ctx_type, ctx_value, ctx_traceback):
+        self.file.close()
+
+with CustomOpen('file') as f:
+    contents = f.read()
