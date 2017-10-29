15222683ba58d27485138be62147c331d5b520ec
hanzhihong9 <hanzhihong9@gmail.com>
2017-10-29 13:08:38 -0400

    Create memory-usage.py

---
+++ memory-usage.py
@@ -0,0 +1,7 @@
+
+'''
+IPython tool to report memory usage deltas for every command you type. 
+If you are running out of RAM 
+then use this tool to understand what's happening. 
+It also records the time spent running each command.
+'''
