5c258c9aff427c165999292d01bf1c786505d77f
hanzhihong9 <hanzhihong9@gmail.com>
2017-10-30 07:37:48 -0400

    Create use-assert.py

---
+++ use-assert.py
@@ -0,0 +1,9 @@
+'''
+use assert or not,
+why use it? is hard to make a decision :  what kind of error it is should make ne use assert
+how to use is very easy
+'''
+
+has_not_error_which_cannot_recover = False ## but what knid of a thing it is?  
+assert( has_not_error_which_cannot_recover, "you can not recover from this kind of error , `
+   just stop and quit the program, after fix this problem, run the progam again ")
