d5d84b8adfa40e85de218f0799c73a8db0df6a24
hanzhihong9 <hanzhihong9@gmail.com>
2017-10-30 07:57:43 -0400

    Update use-async.py

--- use-async.py
+++ use-async.py
@@ -7,3 +7,36 @@ con-currency
 
 aysnc await
 '''
+
+'''
+suspend and resume
+
+1 Callback
+2 generator 
+3 async and await
+4 greenlets package
+5 event loop
+
+'''
+
+import asyncio
+
+loop  = asyncio.get_event_loop()
+
+@asyncio.coroutine
+def hello():
+  print 1
+  yield from asyncio.sleep(3)
+  print 'done'
+
+
+asunc def hello2():
+  print 1
+  await asyncio.sleep(3)
+  print 'done'
+
+if  __name__ == '__main__'
+  loop  = asyncio.run_until_complete( hello() )
+  
+  
+  
